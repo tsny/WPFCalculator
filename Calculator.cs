1cd30674b59bd17e8841cc8a99f6a98be859f9cc
tsny <tsny700@gmail.com>
2018-07-21 21:39:58 -0500

    Init Commit

---
+++ Calculator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCalculator
+{
+    public enum Operand
+    {
+        addition,
+        subtraction, 
+        multiplication,
+        division,
+        power
+    }
+
+    class Calculator
+    {
+        public string inputString;
+
+        public float previousInput;
+        public float currentInput = 0;
+
+        public Operand currentOperand;
+
+        public bool currentInputHasDecimal;
+        public bool hasInput;
+
+        public void InputDigit(float digit)
+        {
+            inputString += digit;
+            currentInput = float.Parse(inputString);
+        }
+
+        public void InputDecimal()
+        {
+            if (currentInputHasDecimal) return;
+
+            inputString += ".";
+            currentInputHasDecimal = true;
+        }
+
+        public void InputOperand(Operand operand)
+        {
+
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CSharpCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Calculator calculator;
+
+        public MainWindow()
+        {
+            calculator = new Calculator();
+            InitializeComponent();
+        }
+
+        private void DigitClick(Object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+
+            if (button == null) return;
+
+            calculator.InputDigit(float.Parse(button.Content.ToString()));
+            InputBox.Text = calculator.currentInput.ToString();
+        }
+    }
+}
